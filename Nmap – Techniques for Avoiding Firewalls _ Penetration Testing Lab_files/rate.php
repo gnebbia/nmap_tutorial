23d9089c14057ea83a593ab58211eb515c4e4903
gnebbia <526473+gb96@users.noreply.github.com>
2019-01-30 17:04:58 +0100

    Added Notes on SCADA

---
+++ Nmap – Techniques for Avoiding Firewalls _ Penetration Testing Lab_files/rate.php
@@ -0,0 +1,6 @@
+PDRTJS_5556500_post_596.avg_rating = 4.5;
+PDRTJS_5556500_post_596.votes = 18;		
+PDRTJS_settings_5556500_post_596= {"type" : "stars","size" : "sml","star_color" : "yellow","custom_star" : "","font_size" : "","font_line_height" : "16px","font_color" : "","font_align" : "left","font_position" : "right","font_family" : "","font_bold" : "normal","font_italic" : "normal","text_vote" : "Vote","text_votes" : "Votes","text_rate_this" : "Rate This","text_1_star" : "Very Poor","text_2_star" : "Poor","text_3_star" : "Average","text_4_star" : "Good","text_5_star" : "Excellent","text_thank_you" : "Thank You","text_rate_up" : "Rate Up","text_rate_down" : "Rate Down"};
+PDRTJS_5556500_post_596.init();		
+PDRTJS_5556500_post_596.token='802e43970c8f9b7564d0be305b11fc95';
+/*5556500,_post_596,wp-post-596,90571152,4.5-18*/
